53f6691796b78b748c9370f88f9399284a573c38
oracle <oracle@GBTC441R12RH.montgomerycollege.edu>
2016-11-12 10:07:29 -0500

    first commit

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -9,16 +9,16 @@ public class FizzBuzz {
 		for (int i = 1; i < 101; i++){
 			if ( i % 3 == 0 && i % 5 == 0 ) {
 				print("FizzBuzz");
-				)else if (i % 3 == 0) {
+			}else if (i % 3 == 0) {
 					print("Fizz");
-					) else if (i % 5 == 0){
+			} else if (i % 5 == 0){
 						print("Buzz");
-						)else { 
-							print(Interger.toString(i));
+			}else { 
+							print(Integer.toString(i));
 						}
 					}
 				}
-		public static String print(String s){
+		private static void print(String s){
 			System.out.println(s);
 		}
 	}
